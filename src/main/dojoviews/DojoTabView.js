6da447bcf78e65ee097415ad95543ebf49e7d5b8
Scott Smith <scott.smith@atsid.com>
2013-05-08 13:25:28 -0400

    Commented out setting the title since it is taking the title for the last tab and setting it to the active tab.  Don't think this is needed here

--- src/main/dojoviews/DojoTabView.js
+++ src/main/dojoviews/DojoTabView.js
@@ -17,7 +17,7 @@ define([
 
         addChild: function (view) {
             this.inherited(arguments);
-            this.set("title", view.title);
+            //this.set("title", view.title);
         },
 
         removeChild: function (childView) {
