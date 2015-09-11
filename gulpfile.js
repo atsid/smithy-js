bd15f4b1a0b953ba1336a515d346888f2ef177f7
David Tittsworth <stopyoukid@gmail.com>
2015-09-11 09:43:33 -0700

    Removed some files from codeclimate since they aren't ours, and added a versioning task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -45,3 +45,33 @@ gulp.task('build', function (callback) {
 
 // The default task (called when you run `gulp` from cli)
 gulp.task('default', ['lint', 'test']);
+
+
+// Versioning tasks
+/**
+ * Increments a version value within the package json and bower json
+ */
+function inc(importance) {
+    var git = require('gulp-git'),
+        bump = require('gulp-bump'),
+        filter = require('gulp-filter'),
+        tag_version = require('gulp-tag-version');
+
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+        // bump the version number in those files
+        .pipe(bump({type: importance}))
+        // save it back to filesystem
+        .pipe(gulp.dest('./'))
+        // commit the changed version number
+        .pipe(git.commit('bumps package version'))
+
+        // read only one file to get the version number
+        .pipe(filter('package.json'))
+        // **tag it in the repository**
+        .pipe(tag_version());
+}
+
+gulp.task('patch', function() { return inc('patch'); })
+gulp.task('feature', function() { return inc('minor'); })
+gulp.task('release', function() { return inc('major'); })
